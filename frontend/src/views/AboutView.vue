2a337299854588a00c7d7cd8d650a2dc23b010cf
André Wakugawa <andre.wakugawa@outlook.com>
2024-08-27 21:07:14 -0300

    Update AboutView.vue

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,15 +1,52 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 id="eae"> eae cebola {{ nome }}</h1>
+
+    <p>
+      <label for="nome" id="labelName">Nome:</label>
+    <input id="nome" type="text" v-model="nome">
+    <input type="text" :value="nome" @input="alteraNome">
+
+    </p>
+    <p v-if="nome.length<5">ELIZABERTO GODOFREDO</p>
+    <p v-else>NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO</p>
+
+    <table>
+      <thead>
+        <th>
+          Nome
+          Senha
+        </th>
+      </thead>
+      <tbody>
+        <tr v-for="usuario in usuarios">
+          <td>{{usuario.nome}}</td>
+          <td>{{usuario.senha}}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+
+
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+  
+const nome = ref("JOVEM")
+
+const usuarios = ref([{nome: "JEFERSON", senha:"123456"}, {nome: "CAMINHOES", senha:"EUROTRUCK"}])
+
+function alteraNome(e: any) {
+  nome.value = e.target.value;
 }
-</style>
+
+function addUser() {
+
+}
+
+
+
+</script>
