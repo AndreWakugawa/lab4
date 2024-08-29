194ad3d4c22b2523bd135516c661852124b45cbd
André Wakugawa <andre.wakugawa@outlook.com>
2024-08-28 22:19:02 -0300

    vue

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="about">
-    <h1 id="eae"> eae cebola {{ nome }}</h1>
+    <h1 id="eae"> Eae</h1>
 
     <p>
       <label for="nome" id="labelName">Nome:</label>
     <input id="nome" type="text" v-model="nome">
     <input type="text" :value="nome" @input="alteraNome">
-
     </p>
-    <p v-if="nome.length<5">ELIZABERTO GODOFREDO</p>
-    <p v-else>NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO</p>
+    <p>
+      <label for="senha" id="labelName">Senha:</label>
+    <input id="senha" type="text" v-model="senha">
+    <input type="text" :value="nome" @input="alteraNome">
+    </p>
+    <button @click="addUser">Incluir</button>
 
     <table>
       <thead>
@@ -35,7 +38,8 @@
 import { ref } from 'vue';
 
   
-const nome = ref("JOVEM")
+const nome = ref("JOVEM");
+const senha = ref("sadboi");
 
 const usuarios = ref([{nome: "JEFERSON", senha:"123456"}, {nome: "CAMINHOES", senha:"EUROTRUCK"}])
 
---
+++ frontend/src/views/UsuarioView.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="usuario">
+      <h1 id="eae"> Eae usuario</h1>
+  
+      <p>
+        <label for="nome" id="labelName">Nome:</label>
+      <input id="nome" type="text" v-model="nome">
+      <input type="text" :value="nome" @input="alteraNome">
+      </p>
+      <p>
+        <label for="senha" id="labelName">Senha:</label>
+      <input id="senha" type="text" v-model="senha">
+      <input type="text" :value="nome" @input="alteraNome">
+      </p>
+      <button @click="addUser">Incluir</button>
+  
+      <table>
+        <thead>
+          <th>
+            Nome
+            Senha
+          </th>
+        </thead>
+        <tbody>
+          <tr v-for="usuario in usuarios">
+            <td>{{usuario.nome}}</td>
+            <td>{{usuario.senha}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
+  
+  
+  
+  
+  <script setup lang="ts">
+  import axios from 'axios';
+import { ref } from 'vue';
+  
+    
+  const nome = ref("JOVEM");
+  const senha = ref("sadboi");
+  
+  const usuarios = ref([{nome: "JEFERSON", senha:"123456"}, {nome: "CAMINHOES", senha:"EUROTRUCK"}])
+  
+  function alteraNome(e: any) {
+    nome.value = e.target.value;
+  }
+  
+  async function getUsers() {
+    axios.get('')
+  }
+  
+  
+  
+  </script>
+  
